c4eff5b03df0a319b1908d861990e2cc3a6ce993
Bas <bas.bolmerISC@nl.ibm.com>
2018-11-20 22:12:24 +0100

    Initial

---
+++ process.py
@@ -0,0 +1,32 @@
+import csv
+import glob
+from collections import defaultdict
+
+files = glob.glob('*.txt')
+pair = defaultdict(list)
+header = ""
+for file in files:
+	header = "%s %s %s" % (header, file, "\t")
+	if not bool(pair):
+		with open(file) as csvfile:
+			csvrows = csv.reader(csvfile, delimiter='\t')
+			next(csvrows)
+			for source, study in csvrows:
+				pair[source].append(study)
+			continue
+			
+	with open(file) as csvfile:
+		csvrows = csv.reader(csvfile, delimiter='\t')
+		next(csvrows)
+		for source, study in csvrows:
+			if source in pair:
+				pair[source].append(study)
+
+# string magic				
+f = open("pair.csv","w+")
+f.write(header + "\n")
+for item in pair:
+	for i in pair[item]:
+		f.write(str(i) + "\t")
+	f.write("\n")
+f.close()
