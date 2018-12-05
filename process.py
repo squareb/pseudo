075731f5e907d996613e048b72cb330516740552
Bas <bas.bolmerISC@nl.ibm.com>
2018-12-05 20:15:59 +0100

    more refactoring and additional test case

--- process.py
+++ process.py
@@ -17,21 +17,20 @@ for file in files:
 	with open(file) as csvfile:
 		csvrows = csv.reader(csvfile, delimiter='\t')
 		next(csvrows)
-		# for each row
+		# for each row, expect the header
 		for id, source, study in csvrows:
-			# if source pseudo is in the dict, and pair is not empty
-			if source in pair and bool(pair):
-				pair[source].append(study)
-			else:
-				pair[source].append(study)
+			pair[source].append(study)
 	
 	csvfile.close()
+
+# only store identifiers that are paired
+pair = [pair[item] for item in pair if len(pair[item]) > 1]
 
 # compose the new file				
 f = open("pair.csv","w+")
 f.write(header + "\n")
 for item in pair:
-	for i in pair[item]:
+	for i in item:
 		f.write(str(i) + "\t")
 	f.write("\n")
 f.close()
